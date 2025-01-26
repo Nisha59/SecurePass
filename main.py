f03ab17c48e31813444bf9b7de052ec5b74bf70c
Nisha59 <nisdc859@gmail.com>
2025-01-26 21:43:26 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from cryptography.fernet import Fernet
+
+
+class PasswordManager:
+
+    def __init__(self):
+        self.key = None
+        self.password_file = None
+        self.password_dict = {}
+
+
+    def create_key(self, path):
+        self.key = Fernet.generate_key()
+        with open (path, 'wb') as f:
+            f.write(self.key)
+
+
+    def load_key(self, path):
+        with open(path, 'rb') as f:
+            self.key = f.read ()
+
+    def create_password_file(self, path, initial_values = None):
+        self.password_file = path
+
+
+        if initial_values is not None:
+            for key, value in initial_values.items():
+                pass
+
+    def load_password_file(self, path):
+        self.password_file = path
+
+
+        with open(path, 'r') as f:
+            for line in f:
+                site, encrypted = line.split(":")
+                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()       
+
+
+
